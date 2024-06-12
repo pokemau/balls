3e4d2393ed68677edc50accf7ba91cf79f678722
pokemau <rentillosa90@gmail.com>
2024-06-13 01:46:28 +0800

    ball

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include "raylib.h"
+#include "raymath.h"
 
-typedef struct ball {
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COUNT 2
+
+typedef struct {
     Vector2 pos;
     Vector2 speed;
     int rad;
 } Ball;
 
 void update(Ball *b);
+void generateBalls(Ball *balls);
+void drawBalls(Ball *balls);
 
 int main(void) {
 
@@ -20,44 +28,150 @@ int main(void) {
 
     SetTargetFPS(60);
 
-    Ball a = {.pos = {10.0f, 10.0f}, .speed = {2, 2}, .rad = 10};
+    Ball *balls = (Ball *)malloc(sizeof(Ball) * COUNT);
+
+    generateBalls(balls);
+
+    Ball a = {.pos = {40.0f, 40.0f}, .speed = {4, 2}, .rad = 20};
 
     Ball b = {.pos = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f},
               .speed = {-2, 1},
-              .rad = 20};
+              .rad = 40};
 
     float gravity = -9.81f;
 
     while (!WindowShouldClose()) {
 
-        update(&a);
-        update(&b);
+        update(balls);
 
         BeginDrawing();
 
         ClearBackground(BLACK);
 
-        DrawCircleV(a.pos, a.rad, ORANGE);
-        DrawCircleV(b.pos, b.rad, RED);
+        drawBalls(balls);
+
+        // TODO: OPTIMIZE COLLISION DETECTION
+        for (int i = 0; i < COUNT; i++) {
+            Ball *curr = &balls[i];
+
+            for (int j = 0; j < COUNT; j++) {
+                Ball *target = &balls[j];
+
+                if (curr == target) {
+                    continue;
+                }
+
+                float distance = Vector2Distance(curr->pos, target->pos);
+
+                if (distance < curr->rad + target->rad) {
+
+                    Vector2 un = Vector2Normalize(
+                        Vector2Subtract(curr->speed, target->speed));
+                    Vector2 ut = {-un.y, un.x};
+
+                    float v1n = Vector2DotProduct(un, curr->speed);
+                    float v1t = Vector2DotProduct(ut, curr->speed);
+
+                    float v2n = Vector2DotProduct(un, target->speed);
+                    float v2t = Vector2DotProduct(ut, target->speed);
+
+                    Vector2 newV1n = Vector2Scale(un, v1n);
+                    Vector2 newV1t = Vector2Scale(ut, v1t);
+
+                    Vector2 newV2n = Vector2Scale(un, v2n);
+                    Vector2 newV2t = Vector2Scale(ut, v2t);
+
+                    curr->speed = Vector2Add(newV1n, newV1t);
+                    target->speed = Vector2Add(newV2n, newV2t);
+
+                    // curr->speed = normal;
+
+                    /*
+                        Vector2 angle = { target->pos.x, target->pos.y };
+                        angle = Vector2Normalize(angle);
+
+                        DrawLine(curr->pos.x, curr->pos.y, target->pos.x,
+                                 target->pos.y, RED);
+
+                        target->speed.x = angle.x;
+                        target->speed.y = angle.y;
+
+                        curr->speed.x = -angle.x;
+                        curr->speed.y = -angle.y;
+                        */
+                }
+            }
+        }
+
+        DrawFPS(10, 10);
 
         EndDrawing();
     }
+
+    free(balls);
 
     CloseWindow();
 
     return 0;
 }
 
-void update(Ball *b) {
+void update(Ball *balls) {
 
-    b->pos.y += b->speed.y;
-    b->pos.x += b->speed.x;
+    for (int i = 0; i < COUNT; i++) {
 
-    if (b->pos.y <= b->rad || b->pos.y >= GetScreenHeight() - b->rad) {
-        b->speed.y *= -1;
+        Ball *curr = &balls[i];
+
+        curr->pos.x += curr->speed.x;
+        curr->pos.y += curr->speed.y;
+
+        if (curr->pos.y <= curr->rad ||
+            curr->pos.y >= GetScreenHeight() - curr->rad) {
+
+            curr->speed.y *= -1;
+        }
+
+        if (curr->pos.x <= curr->rad ||
+            curr->pos.x >= GetScreenWidth() - curr->rad) {
+            curr->speed.x *= -1;
+        }
     }
+}
 
-    if (b->pos.x <= b->rad || b->pos.x >= GetScreenWidth() - b->rad) {
-        b->speed.x *= -1;
+void createBall(Ball *b, Vector2 pos, Vector2 speed, int rad) {
+    b->pos = pos;
+    b->speed = speed;
+    b->rad = rad;
+}
+
+void drawBalls(Ball *balls) {
+
+    for (int i = 0; i < COUNT; i++) {
+        DrawCircleV(balls[i].pos, balls[i].rad, ORANGE);
     }
+}
+
+void generateBalls(Ball *balls) {
+
+    Vector2 pos = {100, 100};
+    Vector2 speed = {2, 2};
+    int rad = 30;
+    createBall(&(balls[0]), pos, speed, rad);
+
+    Vector2 pos2 = {200, 200};
+    Vector2 speed2 = {-2, -2};
+
+    createBall(&(balls[1]), pos2, speed2, rad);
+
+    /*
+    for (int i = 0; i < COUNT; i++) {
+        Vector2 pos = {GetRandomValue(40, GetScreenWidth() - 30),
+                       GetRandomValue(40, GetScreenHeight() - 30)};
+        // Vector2 speed = {GetRandomValue(-2, 2), GetRandomValue(-2, 2)};
+        Vector2 speed = {-2, 2};
+        int rad = GetRandomValue(10, 30);
+
+        createBall(&(balls[i]), pos, speed, rad);
+    }
+    Vector2 speed = {2, 2};
+    */
 }
