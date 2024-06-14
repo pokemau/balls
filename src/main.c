17eb135b9f0c9841bafe56dacb4d13168ca847f3
pokemau <rentillosa90@gmail.com>
2024-06-14 13:10:23 +0800

    bouncing balls

--- src/main.c
+++ src/main.c
@@ -1,20 +1,23 @@
 #include "raylib.h"
 #include "raymath.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define COUNT 2
+#define COUNT 200
+
+Color colors[] = {YELLOW, ORANGE, GREEN, RED};
 
 typedef struct {
     Vector2 pos;
-    Vector2 speed;
+    Vector2 vel;
     int rad;
+    Color color;
 } Ball;
 
 void update(Ball *b);
 void generateBalls(Ball *balls);
 void drawBalls(Ball *balls);
+void handleCollision(Ball *ball1, Ball *ball2);
 
 int main(void) {
 
@@ -23,8 +26,7 @@ int main(void) {
 
     SetConfigFlags(FLAG_MSAA_4X_HINT);
 
-    InitWindow(screenWidth, screenHeight,
-               "raylib [core] example - 3d camera first person");
+    InitWindow(screenWidth, screenHeight, "balls");
 
     SetTargetFPS(60);
 
@@ -32,10 +34,10 @@ int main(void) {
 
     generateBalls(balls);
 
-    Ball a = {.pos = {40.0f, 40.0f}, .speed = {4, 2}, .rad = 20};
+    Ball a = {.pos = {40.0f, 40.0f}, .vel = {4, 2}, .rad = 20};
 
     Ball b = {.pos = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f},
-              .speed = {-2, 1},
+              .vel = {-2, 1},
               .rad = 40};
 
     float gravity = -9.81f;
@@ -65,40 +67,7 @@ int main(void) {
 
                 if (distance < curr->rad + target->rad) {
 
-                    Vector2 un = Vector2Normalize(
-                        Vector2Subtract(curr->speed, target->speed));
-                    Vector2 ut = {-un.y, un.x};
-
-                    float v1n = Vector2DotProduct(un, curr->speed);
-                    float v1t = Vector2DotProduct(ut, curr->speed);
-
-                    float v2n = Vector2DotProduct(un, target->speed);
-                    float v2t = Vector2DotProduct(ut, target->speed);
-
-                    Vector2 newV1n = Vector2Scale(un, v1n);
-                    Vector2 newV1t = Vector2Scale(ut, v1t);
-
-                    Vector2 newV2n = Vector2Scale(un, v2n);
-                    Vector2 newV2t = Vector2Scale(ut, v2t);
-
-                    curr->speed = Vector2Add(newV1n, newV1t);
-                    target->speed = Vector2Add(newV2n, newV2t);
-
-                    // curr->speed = normal;
-
-                    /*
-                        Vector2 angle = { target->pos.x, target->pos.y };
-                        angle = Vector2Normalize(angle);
-
-                        DrawLine(curr->pos.x, curr->pos.y, target->pos.x,
-                                 target->pos.y, RED);
-
-                        target->speed.x = angle.x;
-                        target->speed.y = angle.y;
-
-                        curr->speed.x = -angle.x;
-                        curr->speed.y = -angle.y;
-                        */
+                    handleCollision(curr, target);
                 }
             }
         }
@@ -121,57 +90,73 @@ void update(Ball *balls) {
 
         Ball *curr = &balls[i];
 
-        curr->pos.x += curr->speed.x;
-        curr->pos.y += curr->speed.y;
+        curr->pos.x += curr->vel.x;
+        curr->pos.y += curr->vel.y;
 
         if (curr->pos.y <= curr->rad ||
             curr->pos.y >= GetScreenHeight() - curr->rad) {
 
-            curr->speed.y *= -1;
+            curr->vel.y *= -1;
         }
 
         if (curr->pos.x <= curr->rad ||
             curr->pos.x >= GetScreenWidth() - curr->rad) {
-            curr->speed.x *= -1;
+            curr->vel.x *= -1;
         }
     }
 }
 
-void createBall(Ball *b, Vector2 pos, Vector2 speed, int rad) {
+void createBall(Ball *b, Vector2 pos, Vector2 vel, int rad) {
     b->pos = pos;
-    b->speed = speed;
+    b->vel = vel;
     b->rad = rad;
+    b->color = colors[GetRandomValue(0, 3)];
 }
 
 void drawBalls(Ball *balls) {
 
     for (int i = 0; i < COUNT; i++) {
-        DrawCircleV(balls[i].pos, balls[i].rad, ORANGE);
+        DrawCircleV(balls[i].pos, balls[i].rad, balls[i].color);
     }
 }
 
 void generateBalls(Ball *balls) {
 
-    Vector2 pos = {100, 100};
-    Vector2 speed = {2, 2};
-    int rad = 30;
-    createBall(&(balls[0]), pos, speed, rad);
-
-    Vector2 pos2 = {200, 200};
-    Vector2 speed2 = {-2, -2};
-
-    createBall(&(balls[1]), pos2, speed2, rad);
-
-    /*
     for (int i = 0; i < COUNT; i++) {
-        Vector2 pos = {GetRandomValue(40, GetScreenWidth() - 30),
-                       GetRandomValue(40, GetScreenHeight() - 30)};
-        // Vector2 speed = {GetRandomValue(-2, 2), GetRandomValue(-2, 2)};
-        Vector2 speed = {-2, 2};
-        int rad = GetRandomValue(10, 30);
+        Vector2 pos = {GetRandomValue(80, GetScreenWidth() - 30),
+                       GetRandomValue(80, GetScreenHeight() - 30)};
 
-        createBall(&(balls[i]), pos, speed, rad);
+        Vector2 vel = {1, 1};
+
+        int rad = GetRandomValue(5, 5);
+
+        createBall(&(balls[i]), pos, vel, rad);
     }
-    Vector2 speed = {2, 2};
-    */
+    Vector2 vel = {2, 2};
+}
+
+void handleCollision(Ball *ball1, Ball *ball2) {
+    float dx = ball1->pos.x - ball2->pos.x;
+    float dy = ball1->pos.y - ball2->pos.y;
+    float distance = sqrt(dx * dx + dy * dy);
+
+    distance = Vector2Distance(ball1->pos, ball2->pos);
+
+    float nx = dx / distance;
+    float ny = dy / distance;
+
+    float vx = ball1->vel.x - ball2->vel.x;
+    float vy = ball1->vel.y - ball2->vel.y;
+
+    float dotProduct = vx * nx + vy * ny;
+
+    if (dotProduct > 0)
+        return;
+
+    float impulse = (2 * dotProduct) / (1 + 1);
+
+    ball1->vel.x -= impulse * nx;
+    ball1->vel.y -= impulse * ny;
+    ball2->vel.x += impulse * nx;
+    ball2->vel.y += impulse * ny;
 }
